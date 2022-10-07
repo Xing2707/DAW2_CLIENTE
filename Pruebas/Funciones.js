132713aa6f25b0f5640d2f450a371b33606f2b40
juan <juanantonio@gmail.com>
2022-10-07 20:48:24 +0200

    Tema1

---
+++ Pruebas/Funciones.js
@@ -0,0 +1,13 @@
+function NombreCompreto(nom1,nom2){
+    return nom1 +' '+ nom2;
+}
+
+NombreCompreto("Ana","Maria");
+let nombre1="Ana";
+let nombre2="Maria";
+
+console.log(NombreCompreto("Ana","Maria"));
+
+NombreCompreto(nombre1,nombre2);
+
+console.log(NombreCompreto(nombre1,nombre2));
