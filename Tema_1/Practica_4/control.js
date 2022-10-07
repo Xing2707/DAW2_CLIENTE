b3ad44e5993660674f977bb1c21dd8dfbd03e00f
juan <juanantonio@gmail.com>
2022-10-07 20:06:50 +0200

    Tema1

--- Tema_1/Practica_4/control.js
+++ Tema_1/Practica_4/control.js
@@ -1,6 +1,7 @@
 window.addEventListener("load",function(params){
 let contrasena = document.getElementById("gmail");
 let p1= document.getElementById("parrafo");
+let p2= document.getElementById("parrafo2");
 
 const nombre = this.document.getElementById("user")
 nombre.addEventListener("keyup",function(texto){  
@@ -12,7 +13,7 @@ nombre.addEventListener("keyup",function(texto){
 contrasena.addEventListener("keyup",function(texto){   
     console.log("input")
     console.log("texto: ",texto.target.value)
-    p1.innerHTML=texto.target.value;
+    p2.innerHTML=texto.target.value;
     })
    
 })
