c31799bb794fc1d84c032aac450f835fc985c37c
juan <juanantonio@gmail.com>
2022-10-21 20:52:00 +0200

    Tema3

--- Tema_3/comunicar-ventanas-20221021/comunicacion.js
+++ Tema_3/comunicar-ventanas-20221021/comunicacion.js
@@ -1,18 +1,24 @@
 		var todosLados=null;
 
 		function abrirVentana(){
-			let contenido = document.getElementById("idUrl").value;
+			
 			let vs = window.open("secundaria.html");	
 			
+			
+
 			vs.onload=function(){
-				let caja=document.getElementById("texto");
-				caja.innerHTML=contenido;
-				console.log(contenido);
+
+				//vs.document.getElementById("texto").innerHTML="no nos gusta";
+
 			}
+
 			
 			todosLados="no nos gusta tampoco";
 			
+
 			// Cerrar Venta pasados un 3 segundos
 			//setTimeout(cerrarVentana,3000)
+
+			setTimeout(function(){vs.window.close()},3000);
 			
 		}
