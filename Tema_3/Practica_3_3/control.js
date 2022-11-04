14642594e467c805629b9072dbda0b4c4f4ac7f5
juan <juanantonio@gmail.com>
2022-11-04 16:09:47 +0100

    Modificado

--- Tema_3/Practica_3_3/control.js
+++ Tema_3/Practica_3_3/control.js
@@ -1,7 +1,7 @@
 let nuevo
 function abrirVentana(){
     alert("La ventana Se cierra automaticamente en 30 segunto.\nTambien puedes cerrarlo con el bonto de cierro");
-   nuevo= window.open("https://www.w3schools.com", "", "top=100,left=200 width=400,height=400");  //en firefox no he podido abrir una ventana fija, sera por que firefox lo configura cuanto abre una nueva ventana se abre completa por que en chrome puedo abrir ventana fija.
+   nuevo= window.open("https://www.w3schools.com", "", "top=100,left=200 width=400,height=400"); 
     setTimeout(function(){nuevo.window.close()},30000);
 }
 
