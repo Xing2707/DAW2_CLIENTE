b851b03cd27c0da5c83c3e99ce5abcd74ac3aa9b
juan <juanantonio@gmail.com>
2022-11-18 21:27:36 +0100

    Juego

--- Tema5/Practica_1/Ejercicio_2/control.js
+++ Tema5/Practica_1/Ejercicio_2/control.js
@@ -1,8 +1,10 @@
 let ArrayColor=["#ec7063","#c39bd3","#45b39d","#f39c12","#85929e"];
 let cartas=document.getElementById("cartas");
+GenerarCarta(10);
 
 function GenerarCarta(total){
+    let estilo="float:left; height:200px; width:16%; background-color:aqua; margin:10px;";
     for(let i=0; i<total; i++){
-        cartas.innerHTML+="<div id='"+i+"'></div>"
+        cartas.innerHTML+="<div id='carta"+i+"' style='"+estilo+"'></div>"
     }
 }
